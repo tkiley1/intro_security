1841535a5a19b1ef114da76fa5ed395fc4efcefd
tkiley1 <thomas_kiley1@student.uml.edu>
2019-10-06 18:21:38 -0400

    Initial commit, simple login system and registry

--- login.py
+++ login.py
@@ -6,10 +6,14 @@ import os
 #simple login system
 
 def login():
+    #prompt user for credentials
     uname  = input("Enter your username: ")
+    #get hashed password from user
     passw  = getpass.getpass("Password: ").encode('utf-8')
     passwh = hashlib.sha256(passw).hexdigest()
+    #check directory for matching user
     chkpth = "./users/" + uname
+    #if a matching user exists, compare password hash with stored password hash
     if os.path.exists(chkpth):
         f = open(chkpth + "/password", "r")
         cpasswh = f.read()
@@ -20,14 +24,19 @@ def login():
     return -1
 
 def register():
+    #prompt user for a username
     uname = input("Enter a new username: ")
+    #make sure the username is unique
     while os.path.exists("./users/" + uname):
         uname = input("Username taken.  Enter a new username: ")
+    #create directory for new user
     os.mkdir("./users/" + uname)
+    #get password and store hash
     passw = getpass.getpass("Enter a password: ").encode("utf-8")
     passwh = hashlib.sha256(passw).hexdigest()
     f = open("./users/" + uname + "/password", "w")
     f.write(passwh)
+    
     return
     
     
